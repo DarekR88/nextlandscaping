18a9906a8b570ba26a81395a3bd429e2e2ec11b7
Darek Radke <dradmclad@gmail.com>
2024-03-20 22:27:04 -0500

    home page feature formatting

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,16 @@
 import Image from "next/image";
 import bigyardImg from "../../public/images/bigyardpink.jpg";
+import stonesImg from "../../public/images/stones.jpg";
 
 export default function Home() {
   return (
     <main>
       <div className="flex flex-col lg:flex-row lg:gap-10 max-w-[1200px] m-auto lg:mb-[120px] mb-[50px]">
         <div className="flex flex-col max-w-[400px] m-auto lg:max-w-[500px] px-[20px]">
-          <h2 className="mb-4 text-5xl text-center">
+          <h2 className="mb-4 text-5xl text-center font-Playfair">
             ABOUT <span className="text-button-primary">US</span>
           </h2>
-          <p>
+          <p className="font-Lato text-xl">
             Welcome to Floreo, where outdoor dreams come to life. From luxurious
             outdoor kitchens to charming gazebos and everything in between, our
             expert team is dedicated to crafting stunning outdoor spaces that
@@ -19,13 +20,16 @@ export default function Home() {
             Floreo, your outdoor paradise awaits
           </p>
         </div>
-        <div className="relative lg:w-[750px] lg:h-[350px] overflow-hidden">
-          <Image
-            src={bigyardImg}
-            alt="weights"
-            fill={true}
-            style={{ objectFit: "contain" }}
-          />
+        <div className="flex flex-row lg:w-[750px] lg:h-[350px] overflow-hidden m-auto lg:gap-4">
+          <div className="lg:w-[225px] overflow-hidden">
+            <Image src={bigyardImg} alt="weights" />
+          </div>
+          <div className="lg:w-[225px] overflow-hidden">
+            <Image src={bigyardImg} alt="weights" />
+          </div>
+          <div className="lg:w-[225px] overflow-hidden">
+            <Image src={stonesImg} alt="weights" />
+          </div>
         </div>
       </div>
     </main>
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,10 +8,9 @@ const config: Config = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      fontFamily: {
+        Playfair: ["Playfair Display", "sans-serif"],
+        Lato: ["Lato", "sans-serif"],
       },
     },
   },
