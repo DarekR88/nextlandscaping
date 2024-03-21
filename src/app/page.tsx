74a6e3127bb79330c7ea9fd1d5b75b874b441ca9
Darek Radke <dradmclad@gmail.com>
2024-03-20 22:36:22 -0500

    formatting for home page feature

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,13 @@
 import Image from "next/image";
 import bigyardImg from "../../public/images/bigyardpink.jpg";
 import stonesImg from "../../public/images/stones.jpg";
+import vertwalkImg from "../../public/images/verticlewalk.jpg";
 
 export default function Home() {
   return (
     <main>
       <div className="flex flex-col lg:flex-row lg:gap-10 max-w-[1200px] m-auto lg:mb-[120px] mb-[50px]">
-        <div className="flex flex-col max-w-[400px] m-auto lg:max-w-[500px] px-[20px]">
+        <div className="flex flex-col max-w-[400px] m-auto lg:max-w-[500px] px-[20px] lg:mt-0">
           <h2 className="mb-4 text-5xl text-center font-Playfair">
             ABOUT <span className="text-button-primary">US</span>
           </h2>
@@ -16,16 +17,15 @@ export default function Home() {
             expert team is dedicated to crafting stunning outdoor spaces that
             exceed your expectations. Whether you're envisioning a cozy backyard
             retreat or an expansive garden oasis, we have the knowledge,
-            experience, and creativity to bring your vision to fruition. With
-            Floreo, your outdoor paradise awaits
+            experience, and creativity to bring your vision to fruition.
           </p>
         </div>
-        <div className="flex flex-row lg:w-[750px] lg:h-[350px] overflow-hidden m-auto lg:gap-4">
+        <div className="flex flex-row lg:w-[750px] lg:h-[275px] overflow-hidden m-auto lg:gap-4">
           <div className="lg:w-[225px] overflow-hidden">
             <Image src={bigyardImg} alt="weights" />
           </div>
           <div className="lg:w-[225px] overflow-hidden">
-            <Image src={bigyardImg} alt="weights" />
+            <Image src={vertwalkImg} alt="weights" />
           </div>
           <div className="lg:w-[225px] overflow-hidden">
             <Image src={stonesImg} alt="weights" />
